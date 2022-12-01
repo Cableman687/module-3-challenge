f45839569005701a4b5033622202af2efc14e943
William <will_cable@hotmail.com>
2022-12-01 16:34:05 +0800

    Second Commit - Major updates to js file, containing most submission code. Minor changes to HTML file removing 'test-text' from upper left corner of webpage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,14 +1,134 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//returns the element with the ID of "Generate"
+const generateBtn = document.querySelector("#generate");
 
-  passwordText.value = password;
+//returns the element with the ID of "Password"
+const passwordText = document.querySelector("#password");
+
+
+
+//---------------------Password Arrays--------------------------------------
+
+  const upperArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowerArray = "abcdefghijklmnopqrstuvwxyz";
+  const numericArray = "0123456789"
+  const specialArray = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  //---------------------Request & Validate Password Length----------------------------
+
+function requestLength(){
+  
+  const lengthChoice = window.prompt("How long Would you like your password to be? (Must be a number between 8 and 128");
+
+  // Check if User input for length is a number
+  function NumberCheck(UserNumberChoice){
+    return !isNaN(UserNumberChoice);
+  }
+
+  // Validate length input value is a number, otherwise repeat prompt.
+  if(!NumberCheck(lengthChoice)){
+    return requestLength();
+  }
+
+  const length = lengthChoice;
+
+  // Validate length between 8 and 128, otherwise repeat prompt.
+  if(length < 8 || length > 128){
+    return requestLength();
+  }
+  
+  // Return length value once all criteria are met.
+  return length;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//---------------------------------Request & Return Lowercase Parameter--------------------------
+function requestLower(){
+
+  const lowerChoice = confirm("Would you like to include Lowercase letters? (OK/Cancel)");
+  return lowerChoice;
+    
+}
+
+//---------------------------------Request & Return Uppercase Parameter--------------------------
+function requestUpper(){
+  
+  const upperChoice = confirm("Would you like to include Uppercase letters? (OK/Cancel)");
+  return upperChoice;
+  
+}
+
+//---------------------------------Request & Return Numeric Parameter----------------------------
+
+function requestNumeric(){
+  
+  const numericChoice = confirm("Would you like to include Numbers? (OK/Cancel)");
+  return numericChoice;
+      
+}
+
+//---------------------------------Request & Return Special Character Parameter------------------
+
+function requestSpecial(){
+  
+  const specialChoice = confirm("Would you like to include Special Characters? (OK/Cancel)");
+  return specialChoice;
+     
+  
+}
+
+//-------------------------------------------Password Generation----------------------------------
+
+function generatePassword() {
+
+  //Set variables and Arrays containing the returned value(s) of the above functions
+  var finalPasswordLength = requestLength();  
+  var combinedCriteria = [requestLower(), requestUpper(), requestNumeric(), requestSpecial()];  
+  var result = "";
+
+  //Add to the result 'pool' of potential characters using the boolean values produced in the Password Arrays
+  if(combinedCriteria[0]) {
+    result = result  + lowerArray;
+  }
+  if(combinedCriteria[1]) {
+    result = result  + upperArray;
+  }
+  if(combinedCriteria[2]) {
+    result = result  + numericArray;
+  }
+  if(combinedCriteria[3]) {
+    result = result  + specialArray;
+  }
+
+  //set password starting-state to empty string ("")   
+  var finalpassword = "";
+
+  //With each iteration of the 'For' loop, add a random element to the 'finalpassowrd' string, until the predetermined length is reached.
+  for(var i = 0; i < finalPasswordLength; i++){
+
+    var randomElement = result[Math.floor(Math.random() * result.length)];
+
+    finalpassword = finalpassword + randomElement;
+  }
+ 
+  //set the value property in HTML
+  passwordText.value = finalpassword;
+    
+
+}
+
+//Trigger Password Generation Function on click event
+generateBtn.addEventListener("click", generatePassword);
+
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # module-3-challenge
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- My motivation to complete this challenge was to test my own skills gained in the most recent week of the coding bootcamp, including but not limited to; Logical Comparison Operators, Conditional Statements, Arrays, Iteration and many others.
+- This project was built as a testing ground for my own retention of the aforementioned skills.
+- This challenge allows users to generate unique, robust passwords with a set of criteria agreed to by the user. This solves the problem of having to think of one yourself, and ensures that the password is non-guessable and random.
+- Through building this application, I learnt that there are many programming skills outside of what we learn throughout the week, and that research and collaboration is critical in order to achieve the best outcome for each project.
+
+![Screenshot](/assets/images/password-generator-screenshot.png)
+
+## Usage
+
+
+
+This respository is to be accessed by the user in order to generate unique passwords with specific criteria met.
+
+To use the Password Generation Tool , the user must click the "Generate Password" button, and answer each prompt with either a value, 'OK', or 'Cancel' selection.
+
+## Credits
+
+Random Number Generation Techniques:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+
+Number Entry Validation Techniques:
+https://mkyong.com/javascript/check-if-variable-is-a-number-in-javascript/
+
+
+
+## License
+
+MIT
+
+
+
+## How to Contribute
+
+This repository is not open for public contribution.
